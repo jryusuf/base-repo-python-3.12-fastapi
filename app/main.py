927e1d87a9c3115e4bce57ff0539a51e8811d571
Yusuf Yuksel <yusufyuksel@aol.com>
2024-02-05 15:24:29 +0000

    Add print statement to main function

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ def main():
     # Your code here
 
     if __name__ == "__main__":
+        print("Hello, World!")
         print(
             "Hello, World!Hello, World!Hello, World!Hello, World!Hello, World!Hello, World!Hello, World!Hello, World!Hello, World!Hello, World!Hello, World!"
         )
